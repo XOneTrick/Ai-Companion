fd37c599bce58848992e82cf19a9af8de16daca8
XOneTrick <68628946+XOneTrick@users.noreply.github.com>
2023-08-06 21:43:17 +1200

    Dashboard and Navbar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,48 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
+
+import { Card } from "@/components/ui/card";
+import { MessageSquareIcon } from "lucide-react";
+
+const tools = [
+  {
+    label: "Conversation",
+    icon: MessageSquareIcon,
+    color: "text-violet-500",
+    bgColor: "bg-viloet-500/10",
+    href: "/conversation",
+  }
+]
 
 const DashboardPage = () => {
   return (
     <div>
-    <p>Dashboard Page (Protected)</p>
-    <UserButton afterSignOutUrl="/"/>
+      <div className="mb-8 space-y-4">
+        <h2 className="text-2xl md:text-4xl font-bold text-center">
+          Explore the power of AI
+        </h2>
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+          Chat the Stroud Bot, a not so smart AI
+        </p>
+      </div>
+      <div className="px-4 md:px-20 lg:px-32 space-y-4">
+        {tools.map((tool) => (
+          <Card 
+            key={tool.href}
+            className="
+              p-4 
+              border-black/5 
+              flex items-center 
+              justify-between 
+              hover:shadow-md 
+              transition 
+              cursor-pointer
+              ">
+            
+          </Card>
+        ))}
+      </div>
     </div>
   );
-  
-}
+};
 
 export default DashboardPage;
